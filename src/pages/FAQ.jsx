4b0a20adee30dfb4006f89db84b1e4fe00f85ee5
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-10-05 17:47:28 +0530

    adding the responsive ui of faq

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -17,46 +17,46 @@ const FAQ = () => {
   const [isFaqShow5, setIsFaqShow5] = useState(false);
   const [isFaqShow6, setIsFaqShow6] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     window.scrollTo(0, 0);
-  },[])
+  }, []);
   return (
     <div className="w-full flex items-center justify-center flex-col">
       <div className="banner w-full flex items-center justify-center">
         <img
           src={FaqBanner}
           alt="products-photo"
-          className="w-screen h-[479px] object-cover object-center overflow-hidden"
+          className="w-screen md:h-[479px] h-[300px] object-cover object-center overflow-hidden"
         />
         <div className="absolute bg-white flex items-center justify-center px-16 py-3">
-          <h5 className="text-3xl font-bold text-center">FAQ</h5>
+          <h5 className="md:text-3xl text-xl font-bold text-center">FAQ</h5>
         </div>
       </div>
-      <div
-        className="w-8/12 rounded-3xl py-6 px-6 flex items-center flex-col justify-center relative top-[-50px] bg-[#FFD600] shadow-lg"
-       
-      >
-        <h1 className="text-center text-black text-4xl font-semibold">
+      <div className="md:w-8/12 w-11/12 rounded-3xl py-6 md:px-6 px-3 flex items-center flex-col justify-center relative top-[-50px] bg-[#FFD600] shadow-lg">
+        <h1 className="text-center md:block hidden text-black text-4xl font-semibold">
           Frequently Asked Question{" "}
         </h1>
-        <p className="text-center text-xl text-gray ">
-        Get Answers to Your Common Queries.How does Le.
+        <h1 className="text-center md:hidden  text-black md:text-4xl text-2xl font-semibold">
+          FAQ{" "}
+        </h1>
+        <p className="text-center md:text-xl text-lg text-gray ">
+          Get Answers to Your Common Queries.How does Learn.
         </p>
       </div>
 
-      <div className="w-full flex flex-col items-center justify-center mt-16 relative">
-        <div className="w-11/12 flex justify-between relative mt-16">
+      <div className="w-full flex flex-col items-center justify-center md:mt-16 mt-0 relative">
+        <div className="md:w-11/12 w-full flex md:flex-row flex-col justify-between relative md:mt-16 mt-0">
           <div className="absolute left-[-70px] top-[-70px] h-[200px] w-[200px] -z-10">
             <img src={productSideDesign} alt="" className="w-full h-full" />
           </div>
-          <div className="w-4/12 p-8">
+          <div className="w-4/12 p-8 md:flex hidden">
             <img
               src={frequentlyAskedImg}
               alt=""
               className="rounded-3xl h-[600px] w-full"
             />
           </div>
-          <div className="w-7/12">
+          <div className="md:w-7/12 w-full md:px-0 px-3 md:mb-0 mb-8">
             <div className="faq-item w-full my-4">
               <div
                 className={`faq-question shadow-xl transition-all p-4 cursor-pointer  border-[1px] border-[#969696] flex items-center justify-between ${
@@ -222,7 +222,7 @@ const FAQ = () => {
             </div>
           </div>
         </div>
-        <div className="absolute bottom-[-50px] right-[-50px] h-[150px] w-[150px]">
+        <div className="absolute bottom-[-50px] right-[-50px] h-[150px] w-[150px] md:flex hidden">
           <img src={yellowStar} alt="star" className="h-full w-full" />
         </div>
       </div>
